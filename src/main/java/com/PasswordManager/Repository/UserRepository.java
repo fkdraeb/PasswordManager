ae8d7efbd7a0b4a0beb223a9d7f7fa2b6886749b
draeb <tiago.woa@gmail.com>
2022-11-10 10:28:45 +0000

    list users page - created register new user - all ok log in - all ok

--- src/main/java/com/PasswordManager/Repository/UserRepository.java
+++ src/main/java/com/PasswordManager/Repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.PasswordManager.Repository;
 
 import com.PasswordManager.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
 
 }
