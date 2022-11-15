fb663e0582fe63e78be0390a155f63a1ca989712
draeb <tiago.woa@gmail.com>
2022-11-15 11:35:36 +0000

    html code updated, scripts updated

--- src/main/java/com/PasswordManager/Controller/AccountController.java
+++ src/main/java/com/PasswordManager/Controller/AccountController.java
@@ -2,9 +2,11 @@ package com.PasswordManager.Controller;
 
 import com.PasswordManager.Entity.Account;
 import com.PasswordManager.Repository.AccountRepository;
+import com.PasswordManager.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,9 @@ public class AccountController {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    AccountService accountService;
 
     @GetMapping("/")
     public ModelAndView homePage(ModelAndView modelAndView) {
@@ -48,6 +53,21 @@ public class AccountController {
         modelAndView.setViewName("accounts.html");
         return modelAndView;
     }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editAccount(@PathVariable("id") int id, ModelAndView modelAndView) {
+        accountRepository.editAccount(id, "www.asdasdsa.pt", "123456");
+        modelAndView.setViewName("redirect:/accounts");
+        return modelAndView;
+    }
+
+    @PostMapping("/update")
+    public ModelAndView realEditAccount (Account account, ModelAndView modelAndView) {
+        accountService.updateAccount(account);
+        modelAndView.setViewName("redirect:/");
+        return modelAndView;
+    }
+
 
     @GetMapping("/delete/{id}")
     public ModelAndView deleteAccount(@PathVariable("id") int id, ModelAndView modelAndView) {
--- src/main/java/com/PasswordManager/Service/AccountService.java
+++ src/main/java/com/PasswordManager/Service/AccountService.java
@@ -1,8 +1,11 @@
 package com.PasswordManager.Service;
 
+import com.PasswordManager.Entity.Account;
 import com.PasswordManager.Repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -11,5 +14,11 @@ public class AccountService {
     AccountRepository accountRepository;
 
 
-
+    public Account updateAccount(Account account) {
+        Optional<Account> result = accountRepository.findById(account.getId());
+        Account existing = result.get();
+        existing.setPassword(account.getPassword());
+        existing.setWebsite(account.getWebsite());
+        return accountRepository.save(existing);
+    }
 }
